7f834a7c75ef4c01373942dae7f617cedb3e0d3c
JakeBSU <46795426+JakeBSU@users.noreply.github.com>
2021-07-01 17:07:48 -0600

    js

--- scoring.js
+++ scoring.js
@@ -1,1 +1,13 @@
-AustinMazePortfolio
+validate() {
+
+}
+score() {
+	var ele = document.getElementsByName('q1');
+              
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked)
+                document.getElementById("result").innerHTML
+                        = "Gender: "+ele[i].value;
+            }
+
+}
